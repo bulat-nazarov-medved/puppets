128c2fc70e1ac7aa253c6cc6eb3a02b2cbde3737
Bulat Nazarov <bulat.nazarov.medved@gmail.com>
2014-05-03 00:10:45 +0400

    Database upgrade/downgrade

--- resources/sql/nil-0/upgrade.sql
+++ resources/sql/nil-0/upgrade.sql
@@ -2,3 +2,6 @@ create table keys (
   key varchar (255) primary key not null,
   value integer
 );
+
+insert into keys values ('version', 0);
+
--- src/puppets/server/database.clj
+++ src/puppets/server/database.clj
@@ -2,6 +2,8 @@
   (:use
    [korma core db])
   (:require
+   [clojure.java.io :as io]
+   [clojure.java.jdbc.deprecated :as sql]
    [clojure.string :as str]
    [puppets.server.korma :as pk])
   (:import
@@ -19,7 +21,73 @@
                           :props {:ssl true
                                   :sslfactory "org.postgresql.ssl.NonValidatingFactory"}})))
 
-(defentity users
-  (table "users")
-  (pk "id"))
+(def structure-version 0)
 
+(defentity dbkey
+  (table "keys")
+  (pk :key))
+
+;;; Entities
+
+(defentity user
+  (table "users")
+  (pk :id))
+
+;; DB functions
+
+(defn find-key
+  [key]
+  (with-exception-redirect
+    (:value (first (select dbkey (where {:key key}))))))
+
+;;;DB upgrade/downgrade
+
+(defmacro with-exception-redirect [& body]
+  `(try
+     ~@body
+     (catch Exception e#
+       )))
+
+(defn next-version [version]
+  (if version
+    (+ version 1)
+    0))
+
+(defn previous-version [version]
+  (if (or (not version) (= version 0))
+    nil
+    (- version 1)))
+
+(defn execute-sql-file
+  [sql-resource]
+  (sql/with-connection (get-connection db)
+    (sql/transaction (sql/do-prepared (slurp (io/resource sql-resource))))))
+
+(defn structure-upgrade-step
+  [version-from]
+  (let [upgrade-file (str "sql/"
+                          (or version-from "nil") "-" (next-version version-from)
+                          "/upgrade.sql")]
+    (execute-sql-file upgrade-file)))
+
+(defn structure-downgrade-step
+  [version-from]
+  (let [downgrade-file (str "sql/"
+                            (or (previous-version version-from) "nil") "-" version-from
+                            "/downgrade.sql")]
+    (execute-sql-file downgrade-file)))
+
+(defn perform-structure-steps
+  [version step-function]
+  (let [current-version (find-key "version")]
+    (when (not= version current-version)
+      (step-function current-version)
+      (perform-structure-steps version step-function))))
+
+(defn upgrade-structure-to
+  [version]
+  (perform-structure-steps version structure-upgrade-step))
+
+(defn downgrade-structure-to
+  [version]
+  (perform-structure-steps version structure-downgrade-step))
