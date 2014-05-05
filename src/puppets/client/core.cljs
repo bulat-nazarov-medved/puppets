b316810a6665ae876f8c9d377c8bb8d4658b9bd4
Bulat Nazarov <bulat.nazarov.medved@gmail.com>
2014-05-05 17:12:34 +0400

    Basic templating.

--- src/puppets/client/core.cljs
+++ src/puppets/client/core.cljs
@@ -2,7 +2,9 @@
   (:require [enfocus.core :as ef])
   (:require-macros [enfocus.macros :as em]))
 
+(em/deftemplate main-html "/prototype/main.html" [])
+
 (defn init []
-  (ef/at "body" (ef/content "Hello world!!")))
+  (ef/at "body" (ef/content (main-html))))
 
 (set! (.-onload js/window) #(em/wait-for-load (init)))
